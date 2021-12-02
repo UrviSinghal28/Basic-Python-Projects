9b4e53437f33634e5d75a98e231692ef79a9570e
UrviSinghal28 <urvisinghal13@gmail.com>
2021-12-02 11:02:56 +0530

    meh

---
+++ quiz.py
@@ -0,0 +1,68 @@
+quizBegin=input('Are you ready for the quiz?\n')
+
+if quizBegin.lower()!='yes':
+    quit()
+
+print("To quit the game at any moment just type 'quit' as the answer to any question")
+print("Great! Let's Begin")
+score=0
+ques=0
+
+question=input("Question 1: What is the name of the main antagonist in the Shakespeare play Othello?\n")
+ques+=1
+if question.lower()=='lago':
+    print('Correct!')
+    score+=1
+elif question.lower()=='quit':
+    print('Thank you for playing!')
+    quit()
+else:
+    print('Incorrect!')
+
+question=input("Question 2: What element is denoted by the chemical symbol Sn in the periodic table?\n")
+ques+=1
+if question.lower()=='tin':
+    print('Correct!')
+    score+=1
+elif question.lower()=='quit':
+    print('Thank you for playing!')
+    quit()
+else:
+    print('Incorrect!')
+
+question=input("Question 3: What is the smallest planet in our solar system?\n")
+ques+=1
+if question.lower()=='mercury':
+    print('Correct!')
+    score+=1
+elif question.lower()=='quit':
+    print('Thank you for playing!')
+    quit()
+else:
+    print('Incorrect!')
+
+question=input("Question 4: From what grain is the Japanese spirit Sake made?\n")
+ques+=1
+if question.lower()=='rice':
+    print('Correct!')
+    score+=1
+elif question.lower()=='quit':
+    print('Thank you for playing!')
+    quit()
+else:
+    print('Incorrect!')
+
+question=input("Question 5: Which Disney Princess called Gus and Jaq friends?\n")
+ques+=1
+if question.lower()=='cinderella':
+    print('Correct!')
+    score+=1
+elif question.lower()=='quit':
+    print('Thank you for playing!')
+    quit()
+else:
+    print('Incorrect!')
+
+print(f"You got {score} questions correct out of {ques} questions\nThank you for playing!")
+
+
--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,45 +1,50 @@
 import random
 
-def game(computer,playerChoice):
-    if computer==playerChoice:
+print('Lets play a game of rock, paper and scissors!\n')
+
+
+
+def game(computer, playerChoice):
+    if computer == playerChoice:
         return None
 
-    elif computer=="r":
-        if playerChoice=="s":
+    elif computer == "r":
+        if playerChoice == "s":
             return False
-        elif playerChoice=="p":
+        elif playerChoice == "p":
             return True
 
-    elif computer=="p":
-        if playerChoice=="r":
+    elif computer == "p":
+        if playerChoice == "r":
             return False
-        elif playerChoice=="s":
-            return True
-    
-    elif computer=="s":
-        if playerChoice=="p":
-            return False
-        elif playerChoice=="r":
+        elif playerChoice == "s":
             return True
 
-randomNumber=random.randint(1,3)
-if randomNumber==1:
-    computer="r"
-elif randomNumber==2:
-    computer="p"
-elif randomNumber==3:
-    computer="s"
+    elif computer == "s":
+        if playerChoice == "p":
+            return False
+        elif playerChoice == "r":
+            return True
 
-print("computer's turn: Rock(r), Paper(p) or Scissor(s)?")
-playerChoice=input("Your turn: Rock(r), Paper(p) or Scissor(s)?\nYou chose: ")
-print("computer chose:",str(computer))
 
-gameWin=game(computer,playerChoice)
+randomNumber = random.randint(1, 3)
+if randomNumber == 1:
+    computer = "r"
+elif randomNumber == 2:
+    computer = "p"
+elif randomNumber == 3:
+    computer = "s"
 
-if gameWin==None:
-    print("The game is a tie")
+print("Computer's turn: Rock(r), Paper(p) or Scissor(s)?")
+playerChoice = input(
+    "Your turn: Rock(r), Paper(p) or Scissor(s)?\nYou chose: ")
+print("Computer chose:", str(computer))
+
+gameWin = game(computer, playerChoice)
+
+if gameWin == None:
+    print("The game is a tie\n")
 elif gameWin:
-    print("You win")
+    print("You win\n")
 else:
-    print("You lose")
-
+    print("You lose\n")
